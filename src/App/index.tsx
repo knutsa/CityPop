a6fb6c5b8d31d838412c95d6757bc1dea82843f4
Knut Salomonsson <salomonsson.knut@gmail.com>
2021-09-01 18:11:26 +0200

    style

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -10,12 +10,13 @@ const optionStyle: React.CSSProperties = {
   backgroundColor: "var(--secondary)",
   color: "var(--light)",
   padding: "1rem",
-  borderRadius: "5px"
+  borderRadius: "5px",
+  margin: "auto"
 }
 
 const ChooseMode: React.FC<{setMode: React.Dispatch<React.SetStateAction<Mode>>}> = ({setMode})=>{
-  return <section className="mt-5 container">
-    <div className="row g-5 d-flex justify-content-around ">
+  return <section className="mt-5 pt-md-5 container">
+    <div className="row g-5 d-flex justify-content-around justify-content-md-center">
       <div className="col-md-4">
         <button style={optionStyle} onClick={()=>setMode("searchByLand")}>Search By Land</button>
       </div>
